f7fe028a2f90af765a57b307615bebbba554a83a
Vlad Mettler <vlad@liquidant.com>
2016-09-29 14:40:16 +0100

    all broken

--- web.js
+++ web.js
@@ -1,37 +1,37 @@
-var ApiBuilder = require('claudia-api-builder'),
+var ApiBuilder = require("claudia-api-builder"),
     api = new ApiBuilder();
 
 module.exports = api;
 
 
 function wall_in_range(walls, me, weapon_range){
-    for (q=0; q <= weapon_range; q++){
-        if (me.direction == 'top'){
+    for (q=0; q <= weapon_range; q+=1){
+        if (me.direction == "top"){
             if (is_wall(walls, me.x, me.y-q)){
                 return true;
             }
         }
-        if (me.direction == 'bottom'){
+        if (me.direction == "bottom"){
             if (is_wall(walls, me.x, me.y+q)){
                 return true;
             }
         }
-        if (me.direction == 'left'){
+        if (me.direction == "left"){
             if (is_wall(walls, me.x-q, me.y)){
                 return true;
             }
         }
-        if (me.direction == 'right'){
+        if (me.direction == "right"){
             if (is_wall(walls, me.x+q, me.y)){
                 return true;
             }
         }
     }
-    return false
+    return false;
 }
 
 function is_wall(walls, x, y){
-    for (index = 0; index<walls.length; index++){
+    for (index = 0; index<walls.length; index+=1){
         if ((walls[index].x == x) && (walls[index].y == y)){
             return true
         }
@@ -39,28 +39,71 @@ function is_wall(walls, x, y){
     return false;
 }
 
+function is_boundary(field_width, field_height, me){
+    if (me.direction == "top"){
+        if ((me.y-1) <= 0){
+            return true;
+        }
+    }
+    if (me.direction == "bottom"){
+        if ((me.y+1) >= field_height){
+            return true;
+        }
+    }
+    if (me.direction == "right"){
+        if ((me.x+1) >= field_width){
+            return true;
+        }
+    }
+    if (me.direction == "left"){
+        if ((me.x-1) <= 0){
+            return true;
+        }
+    }
+    return false;
+}
+
 function fire(){
     return {
-        "command": 'fire'
+        "command": "fire"
     };
 }
 
 function move_forward(){
     return {
-        "command": 'forward'
+        "command": "forward"
     };
 }
-api.get('/info', function (request){
+
+function turn_left(){
+    return {
+        "command": "turn-left"
+    };
+}
+
+function turn_right(){
+    return {
+        "command": "turn-right"
+    };
+}
+
+api.get("/info", function (request){
     return {
         "name": "Tom the Tank",
         "owner": "Sean and Vlad"
     };
 });
 
-api.post('/command', function (request){
+api.post("/command", fun)tion (request){
     var walls = request.body.walls;
     var me = request.body.you;
     var weapon_range = request.body.weaponRange;
+    var field_width = request.body.mapWidth;
+    var field_height = request.body.mapHeight;
+
+    if is_boundary(field_width, field_height, me){
+        return turn_left();
+    }
     if (wall_in_range(walls, me, weapon_range)){
         return fire();
     } else {
