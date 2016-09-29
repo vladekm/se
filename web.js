664725b522f97285ddf18c86ccf210b73bafc650
sean.blumenfeld <sean.blumenfeld@essencedigital.com>
2016-09-29 14:44:38 +0100

    enemy

--- web.js
+++ web.js
@@ -64,67 +64,66 @@ function is_boundary(field_width, field_height, me){
 }
 
 function enemy_in_range(enemies, me, weapon_range){
-    //
     for (q=0; q <= weapon_range; q++){
         var has_left_enemy = is_enemy(enemies, me.x-q, me.y)
         var has_right_enemy = is_enemy(enemies, me.x+q, me.y-q)
         var has_top_enemy = is_enemy(enemies, me.x, me.y-q)
         var has_bottom_enemy = is_enemy(enemies, me.x, me.y+q)
 
-        if (me.direction == 'top'){
+        if (me.direction == "top"){
             if (has_top_enemy){
-                return 'fire';
+                return "fire";
             }
             if (has_bottom_enemy){
-                return 'turn-left';
+                return "turn-left";
             }
             if (has_left_enemy) {
-                return 'turn-left';
+                return "turn-left";
             }
             if (has_right_enemy) {
-                return 'turn-right';
+                return "turn-right";
             }
         }
-        if (me.direction == 'bottom'){
+        if (me.direction == "bottom"){
             if (has_top_enemy){
-                return 'turn-left';
+                return "turn-left";
             }
             if (has_bottom_enemy){
-                return 'fire';
+                return "fire";
             }
             if (has_left_enemy) {
-                return 'turn-left';
+                return "turn-left";
             }
             if (has_right_enemy) {
-                return 'turn-right';
+                return "turn-right";
             }
         }
-        if (me.direction == 'left'){
+        if (me.direction == "left"){
             if (has_top_enemy){
-                return 'turn-right';
+                return "turn-right";
             }
             if (has_bottom_enemy){
-                return 'turn-left';
+                return "turn-left";
             }
             if (has_left_enemy) {
-                return 'fire';
+                return "fire";
             }
             if (has_right_enemy) {
-                return 'turn-left';
+                return "turn-left";
             }
         }
-        if (me.direction == 'right'){
+        if (me.direction == "right"){
             if (has_top_enemy){
-                return 'turn-left';
+                return "turn-left";
             }
             if (has_bottom_enemy){
-                return 'turn-right';
+                return "turn-right";
             }
             if (has_left_enemy) {
-                return 'turn-left';
+                return "turn-left";
             }
             if (has_right_enemy) {
-                return 'fire';
+                return "fire";
             }
         }
 
@@ -145,18 +144,6 @@ function is_enemy(enemies, x, y){
 function fire(){
     return {
         "command": "fire"
-    };
-}
-
-function turn_left(){
-    return {
-        "command": 'turn-left'
-    };
-}
-
-function turn_right(){
-    return {
-        "command": 'turn-right'
     };
 }
 
@@ -199,11 +186,11 @@ api.post("/command", fun)tion (request){
     if (wall_in_range(walls, me, weapon_range)){
         return fire();
     };
-    if (enemy_in_range(enemies, me, weapon_range) == 'fire'){
+    if (enemy_in_range(enemies, me, weapon_range) == "fire"){
         return fire();
-    } else if (enemy_in_range(enemies, me, weapon_range) == 'turn_left') {
+    } else if (enemy_in_range(enemies, me, weapon_range) == "turn_left") {
         return turn_left();
-    } else if (enemy_in_range(enemies, me, weapon_range) == 'turn_right') {
+    } else if (enemy_in_range(enemies, me, weapon_range) == "turn_right") {
         return turn_right();
     }
     return move_forward();
